6ad060b6be1ae863547159a7152fc3b5b4f45f07
Steve Swett <sswett@thinkful.com>
2015-01-30 18:57:38 +0000

    Changed Ruby version in gemfile to 2.1.5 and fixed note previews

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,5 +5,10 @@ class Note < ActiveRecord::Base
   validates :content, presence: true
 
   scope :sorted, lambda { order('created_at DESC') }
+  
+  
+  def preview
+    self.content.slice(0..100) << "...(read more)"
+  end
 
 end
